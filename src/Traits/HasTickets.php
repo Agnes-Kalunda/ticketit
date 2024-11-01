0be231bace29b1a9c38b49d13f56f345c7c33010
Agnes <agypeter97@gmail.com>
2024-11-01 11:47:41 +0300

    define Trait methods to check ticket permissions from config. file

--- src/Traits/HasTickets.php
+++ src/Traits/HasTickets.php
@@ -11,4 +11,36 @@ trait HasTickets{
         }
         return $this->hasMany('Ticket\Ticketit\Models\Ticket', 'user_id');
 }
+
+
+    //  get active tickets
+
+    public function activeTickets(){
+        return $this->tickets()->whereNull('completed_at');
+    }
+
+    // get completed tickets
+    public function completedTickets(){
+        return $this->tickets()->whereNotNull('completed_at');
+    }
+
+
+    // check if model can manage ticket-config.file
+    public function canManageTickets(){
+        return $this instanceof \App\User &&
+               config('ticketit.permissions.user.manage_tickets', true);
+
+    }
+
+    public function canCreateTicket(){
+        if ($this instanceof \App\Customer){
+            return config('ticketit.permissions.customer.create_ticket', true);
+        }
+
+        return config ('ticketit.permissions.user.create_ticket', true);
+
+    }
+
+
+
 }
