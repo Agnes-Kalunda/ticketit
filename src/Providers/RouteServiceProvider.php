f456fe40aa738295c5a25b0fef8571c006c757a8
Agnes <agypeter97@gmail.com>
2024-11-08 04:39:27 +0300

    RouteService Provider

---
+++ src/Providers/RouteServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Ticket\Ticketit\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    protected $namespace = 'Ticket\Ticketit\Controllers';
+
+    public function boot()
+    {
+        parent::boot();
+    }
+
+    public function map()
+    {
+        $this->mapWebRoutes();
+    }
+
+    protected function mapWebRoutes()
+    {
+        Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(__DIR__.'/../routes.php');
+    }
+}
